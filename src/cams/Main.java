c194c52d87e7a74fed324ec577347a75cb82588e
Aiden Virabouth <avirabouth5@gmail.com>
2025-12-15 10:49:52 -0800

    Add practice message to Main

--- src/cams/Main.java
+++ src/cams/Main.java
@@ -3,6 +3,8 @@ package cams;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to CAMS!");
+        System.out.println("Change #1 – practicing Git push.");
+
         // Later: call your menu and other logic here
     }
 }
